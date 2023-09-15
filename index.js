64813c3ef9262fc3a3b992ce73aae556b232aa88
Sreerag Parakkal <145093477+Sreerag-Parakkal@users.noreply.github.com>
2023-09-15 11:46:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// document.getElementById("firstTitle").innerHTML = "Updated First Title";
+
+// let res =  document.getElementsByClassName("text");
+
+// console.log(res[0].innerHTML = "Updated Link");
+
+// let res = document.getElementsByTagName("h2");
+
+// let res = document.querySelector(".text").innerHTML = "update title";
+
+// console.log(res);
+let res = document.querySelectorAll("a.text");
+
+console.log(res);
